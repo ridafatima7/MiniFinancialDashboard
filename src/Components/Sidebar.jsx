af801556309afa1abd112de5568f97934fb5ac45
butthina446 <ridafatima7>
2025-05-04 14:00:22 +0500

    4 May 2025

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import Logo from "../assets/Logo.png";
-
+import profile from "../assets/profile.avif"
 const Sidebar = () => {
   return (
     <aside className="w-64 h-screen bg-gray-100 border-r p-4 fixed left-0 top-0 flex flex-col justify-between">
@@ -12,17 +12,16 @@ const Sidebar = () => {
         <nav className="flex flex-col gap-3">
           {[
             { label: "Dashboard", path: "/" },
-            { label: "Transactions", path: "/account/1" },
+            { label: "Transactions", path: "/transaction" },
             { label: "Reports", path: "/reports" },
           ].map(({ label, path }) => (
             <NavLink
               key={label}
               to={path}
               className={({ isActive }) =>
-                `p-2 rounded transition-colors font-medium border-b-2 ${
-                  isActive
-                    ? "bg-[#64A547] text-white border-[#64A547]"
-                    : "bg-[#F3F4F6] hover:bg-[#E6F1FA] text-gray-600 border-transparent hover:text-gray-900"
+                `p-2 rounded transition-colors font-medium border-b-2 ${isActive
+                  ? "bg-[#64A547] text-white border-[#64A547]"
+                  : "bg-[#F3F4F6] hover:bg-[#E6F1FA] text-gray-600 border-transparent hover:text-gray-900"
                 }`
               }
             >
@@ -32,7 +31,12 @@ const Sidebar = () => {
         </nav>
       </div>
       {/* Username Section */}
-      <div className="mt-auto text-left py-4 smallText font-bold">
+      <div className="mt-auto text-left py-4 smallText font-bold flex items-center gap-3">
+        <img
+          src={profile}
+          alt=""
+          className="w-10 h-10 rounded-full object-cover"
+        />
         <span>Rida Fatima</span>
       </div>
     </aside>
